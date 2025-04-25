8ae1963c7b31a24c0f7e18c0d0ed9d7b6f717cc0
Manpreet Singh <manpreetsinghmanidev@gmail.com>
2025-04-25 11:10:37 +0530

    craete new plugin

---
+++ banner-sliders.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+* Plugin Name: Simple Slick Banner Slider
+* Description: WordPress Simple Slick Banner Slider for wordpress, easy to use with help of shortcodes.
+* Version: 1.0.0
+* Author: Manpreet Singh
+**/
+
+if ( ! defined( 'ABSPATH' ) ) {
+     die;
+}
+define( 'MS_SBS_VERSION', '1.0.0' );
+define( 'MS_SBS_TEXT_DOMAIN', 'ms-SBSmonial' );
+define( 'MS_SBS_DIR__NAME', dirname( __FILE__ ) );
+define( 'MS_SBS_EDITING__URL', plugin_dir_url( __FILE__ ) );
+define( 'MS_SBS_EDITING__DIR', plugin_dir_path( __FILE__ ) );
+define( 'MS_SBS_PLUGIN', __FILE__ );
+define( 'MS_SBS_PLUGIN_BASENAME', plugin_basename( MS_SBS_PLUGIN ) );
