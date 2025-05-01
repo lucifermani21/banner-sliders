b6cb751195529ab6ed493fd32b46985e375f5e22
Manpreet Singh <manpreetsinghmanidev@gmail.com>
2025-05-01 14:00:39 +0530

    Add meta

--- snippets/wp-bnrshortcode.php
+++ snippets/wp-bnrshortcode.php
@@ -1,11 +1,14 @@
-<?php $dynamic_slider = $this->meta_data;?>
-<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+<?php $post_id = $this->post_id; $dynamic_slider = $this->meta_data;?>
+<div id="carouselExample<?php echo $post_id;?>" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
         <?php $i = 1;
         foreach( $dynamic_slider as $key => $data ):
         $img_src = wp_get_attachment_url( $data['banner_image'], 'full' );?>
             <div class="carousel-item <?php echo $i == 1 ? 'active' : '' ?>">
                 <img src="<?php echo $img_src;?>" class="d-block w-100" alt="<?php echo $data['banner_title']?>">
+                <div class="carousel-caption">
+                    <h3><?php echo $data['banner_description']?></h3>
+                </div>
             </div>
         <?php $i++;
         endforeach;?>
