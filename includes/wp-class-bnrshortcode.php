6b4b6f9089625e6d7711594cfb152f1bcb268725
Manpreet Singh <manpreetsinghmanidev@gmail.com>
2025-05-01 13:27:56 +0530

    Add shortcode

---
+++ includes/wp-class-bnrshortcode.php
@@ -0,0 +1,26 @@
+<?php 
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class BNRSHORTCODE {
+    
+    var $post_id = '';
+    var $meta_data = array();
+
+    public function __construct() {
+        add_shortcode( 'dynamic_slider', array( $this, 'wp_ms_dynamic_banner_slider' ) );
+    }
+    
+    public function wp_ms_dynamic_banner_slider( $atts ) { 
+        $this->post_id = get_the_ID();
+        $this->meta_data = get_post_meta($this->post_id, 'wp_banner_slider_repeatable_data', true);              
+        ob_start();       
+        $theme_file = get_template_directory().'/dynamic_banner_slider/wp-bnrshortcode.php';
+		if( file_exists( $theme_file ) ){
+			include( $theme_file );
+        } else {
+            include_once( MS_SBS_DIR__NAME . '/snippets/wp-bnrshortcode.php' );
+		}  
+        return ob_get_clean();
+    }
+}
---
+++ snippets/wp-bnrshortcode.php
@@ -0,0 +1,22 @@
+<?php echo "<pre>";var_dump( $this->meta_data )?>
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="http://php82.development.com/manpreet/wordpress/maldonadovallejoimmigrationlaw/development/wp-content/uploads/2016/06/Immigration-Law-Office-banner-1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="http://php82.development.com/manpreet/wordpress/maldonadovallejoimmigrationlaw/development/wp-content/uploads/2016/06/Immigration-Law-Office-banner-1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="http://php82.development.com/manpreet/wordpress/maldonadovallejoimmigrationlaw/development/wp-content/uploads/2016/06/Immigration-Law-Office-banner-1.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
