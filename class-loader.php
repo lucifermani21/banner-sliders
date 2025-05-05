504f0484d028346a712e1088e6968be12243de9f
Manpreet Singh <manpreetsinghmanidev@gmail.com>
2025-05-05 12:22:43 +0530

    class loading issue fix

--- class-loader.php
+++ class-loader.php
@@ -1,27 +1,23 @@
-<?php
+<?php 
 
-if (!defined('ABSPATH')) {
+if ( ! defined( 'ABSPATH' ) ) {
     die;
 }
 
-if (!defined('MS_SBS_EDITING__DIR') || !is_dir(MS_SBS_EDITING__DIR . 'includes/')) {
-    return;
-}
+$classes_files = glob( MS_SBS_EDITING__DIR. 'includes/*.php');
 
-$classes_files = glob(MS_SBS_EDITING__DIR . 'includes/*.php');
 if ($classes_files === false) {
     return;
 }
 
-foreach ($classes_files as $file) {
-    if (!preg_match('/^[a-zA-Z0-9\-]+\.php$/', basename($file))) {
-        continue;
-    }
+$class_array = array();
 
+foreach( $classes_files as $key => $file ){
     include_once $file;
-    $base_name = basename($file, '.php');
-    $class_name = strtoupper(preg_replace('/[^a-zA-Z0-9_]/', '', $base_name));
+    $class_array[] = strtoupper( basename( str_replace(  array( 'wp-class-', '.php' ), '', $file ) ) );
+}
 
+foreach( array_unique( $class_array ) as $class_name ) {
     if (!class_exists($class_name)) {
         continue;
     }
